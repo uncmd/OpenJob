985773b023ca1e2174fc3c5532e3cf8d7072c746
uncmd <1951765200@qq.com>
2023-04-21 13:44:37 +0800

    update

--- src/OpenJob.Worker/Hosting/WorkerHost.cs
+++ src/OpenJob.Worker/Hosting/WorkerHost.cs
@@ -31,32 +31,7 @@ public class WorkerHost : IHostedService
             await Task.Delay(10000, cancellationToken);
         }
 
-        using (var scop = _serviceScopeFactory.CreateScope())
-        {
-            var processors = scop.ServiceProvider.GetRequiredService<IEnumerable<IProcessor>>();
-
-            var req = new ProcessorReq
-            {
-                AppName = WorkerRuntime.AppName,
-                AppId = WorkerRuntime.AppId,
-                ModuleName = WorkerRuntime.Client,
-                Version = WorkerRuntime.Version,
-                Dtls = new List<ProcessorReqDtl>()
-            };
-            foreach (var processor in processors)
-            {
-                var processorType = processor.GetType();
-                req.Dtls.Add(new ProcessorReqDtl
-                {
-                    IsEnabled = ProcessorAttribute.GetProcessorIsEnabled(processorType),
-                    Name = ProcessorAttribute.GetProcessorName(processorType),
-                    Description = ProcessorAttribute.GetProcessorDescription(processorType),
-                    ProcessorInfo = processorType.FullName
-                });
-            }
-
-            await _serverClient.RegisterProcessor(req);
-        }
+        await RegisterProcessor();
 
         await _healthReporter.Start(cancellationToken);
     }
@@ -64,5 +39,38 @@ public class WorkerHost : IHostedService
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 向Server注册Processor
+    /// </summary>
+    /// <returns></returns>
+    private async Task RegisterProcessor()
+    {
+        using var scop = _serviceScopeFactory.CreateScope();
+        var processors = scop.ServiceProvider.GetRequiredService<IEnumerable<IProcessor>>();
+
+        var req = new ProcessorReq
+        {
+            AppName = WorkerRuntime.AppName,
+            AppId = WorkerRuntime.AppId,
+            ModuleName = WorkerRuntime.Client,
+            Version = WorkerRuntime.Version,
+            Dtls = new List<ProcessorReqDtl>()
+        };
+
+        foreach (var processor in processors)
+        {
+            var processorType = processor.GetType();
+            req.Dtls.Add(new ProcessorReqDtl
+            {
+                IsEnabled = ProcessorAttribute.GetProcessorIsEnabled(processorType),
+                Name = ProcessorAttribute.GetProcessorName(processorType),
+                Description = ProcessorAttribute.GetProcessorDescription(processorType),
+                ProcessorInfo = processorType.FullName
+            });
+        }
+
+        await _serverClient.RegisterProcessor(req);
     }
 }
